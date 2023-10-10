72e68f3f2ab489095a0e656f52213dc1a1b9d5f6
nvhung <hungthatsu@gmail.com>
2023-10-11 00:09:31 +0700

    first commit in hung branch

--- shop/products/models.py
+++ shop/products/models.py
@@ -10,9 +10,8 @@ class Category(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(30), unique=True, nullable=False)
 
-
 # class Addproduct(db.Model):
-
+# comment this because i test git commit
 
 with app.app_context():
     db.create_all()
