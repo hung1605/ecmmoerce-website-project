e5f6f839c1fded74349458943fb51c9ae58f00d9
nvhung <hungthatsu@gmail.com>
2023-10-12 23:17:08 +0700

    commit 2: hau het code da duoc hoan thien chi can fix o cac thu vien dac biet la (werkzeug)

--- shop/admin/routes.py
+++ shop/admin/routes.py
@@ -1,33 +1,42 @@
+import time
 from flask import render_template, session, redirect, url_for, request, flash
 
 from shop import app, db, bcrypt
 from .forms import RegistrationForm, LoginForm
 from .models import User
-from shop.products.models import Brand, Category
+from shop.products.models import Addproduct,Category,Brand
 import os
-
-@app.route('/')
-def home():
-    return 'day la trang chu'
-
-# @app.route('/')
-# def admin():
-#     # return 'this is admin page'
-#     if 'email' not in session:
-#         flash(f'Please login first', 'danger')
-#         return redirect(url_for('login'))
-#     # products = addproduct.query.all()
-#     # return render_template('admin/index.html', title='Admin page', products=products)
-
 
 @app.route('/admin')
 def admin():
-    return render_template("admin/index.html", title="Admin")
+    products = Addproduct.query.all()
+    return render_template('admin/index.html', title='Admin page',products=products)
+
+@app.route('/brands')
+def brands():
+    brands = Brand.query.order_by(Brand.id.desc()).all()
+    return render_template('admin/brand.html', title='brands',brands=brands)
+
+
+@app.route('/categories')
+def categories():
+    categories = Category.query.order_by(Category.id.desc()).all()
+    return render_template('admin/brand.html', title='categories',categories=categories)
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegistrationForm(request.form)
-    if request.method == 'POST' and form.validate():
+    username_in_used = User.query.filter_by(username=form.username.data).first() 
+    email_in_used = User.query.filter_by(email=form.email.data).first() 
+
+    if username_in_used or email_in_used:
+        if username_in_used:
+            flash(f'Username {form.username.data} is already in use', 'danger')
+        if email_in_used:
+            flash(f'Email {form.email.data} is already in use', 'danger')
+        return render_template('admin/register.html', form=form, title='Registration page')
+    
+    if request.method == 'POST' and form.validate_on_submit():
         hash_password = bcrypt.generate_password_hash(form.password.data)
         user = User(name=form.name.data,
                     username=form.username.data,
@@ -39,17 +48,16 @@ def register():
         return redirect(url_for('admin'))
     return render_template('admin/register.html', form=form, title='Registration page')
 
-
-@app.route('/login', methods=['GET', 'POST'])
+@app.route('/login', methods=['GET','POST'])
 def login():
-    form = LoginForm(request.form)
-    if request.method == "POST" and form.validate():
+    form = LoginForm()
+    if form.validate_on_submit():
         user = User.query.filter_by(email=form.email.data).first()
         if user and bcrypt.check_password_hash(user.password, form.password.data):
             session['email'] = form.email.data
-            flash(f'Welcome {form.email.data} You are logged in now', 'success')
-            return redirect(request.args.get('next') or url_for('admin'))
+            flash(f'welcome {form.email.data} you are logedin now','success')
+            return redirect(url_for('admin'))
         else:
-            flash(f'Wrong email or password', 'danger')
-    return render_template('admin/login.html', form=form, title="Login Page")
-
+            flash(f'Wrong email and password', 'success')
+            return redirect(url_for('login'))
+    return render_template('admin/login.html',title='Login page',form=form)
